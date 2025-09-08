22c2727d5751e5f013eb3ba3160441ed7de47ff2
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-09-09 00:52:11 +0700

    update

--- PY01033.py
+++ PY01033.py
@@ -7,3 +7,4 @@ for i in range(l, r + 1):
     for z in range(j + 1, r + 1):
       if gcd(i, j) == 1 and gcd(j, z) == 1 and gcd(z, i) == 1:
         print("(" + str(i) + ", " + str(j) + ", " + str(z) + ")")
+        
