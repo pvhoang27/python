11cf578ff630c8390b016a4fe156700f02178573
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-09-09 00:37:22 +0700

    update

---
+++ PY01033.py
@@ -0,0 +1,9 @@
+from math import gcd
+
+l, r = list(map(int, input().split()))
+
+for i in range(l, r + 1):
+  for j in range(i + 1, r + 1):
+    for z in range(j + 1, r + 1):
+      if gcd(i, j) == 1 and gcd(j, z) == 1 and gcd(z, i) == 1:
+        print("(" + str(i) + ", " + str(j) + ", " + str(z) + ")")
