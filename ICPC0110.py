96b889a12fef41cf0d08210e355bebdbc9dbb781
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-09-25 21:56:46 +0700

    update

---
+++ ICPC0110.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  a = list(map(int, input().split()))
+
+  sum = 0
+  # for i in range(n):
+  #   x = int(input())
+  #   a.append(x)
+  a.sort()
+  sum = a[-1] + a[-2] + a[-3]
+  # sum = a[::-1] + a[::-2] + a[::-3]
+  print(sum)
+
