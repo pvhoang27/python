429d130adb7e144427401a155e60abd49ce7bd48
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-09-25 23:39:49 +0700

    update

--- PY01066.py
+++ PY01066.py
@@ -6,12 +6,14 @@ t = int(input())
 for _ in range(t):
   n = input()
   s = n[::-1]
+  ok = 1
   # print(h)
 
   for i in range(1, len(n)):
-     if abs(ord(n[i]) - ord(n[i - 1])) == abs(ord(s[i]) - ord(s[i - 1])):
-       print("YES") 
+     if abs(ord(n[i]) - ord(n[i - 1])) != abs(ord(s[i]) - ord(s[i - 1])):
+       ok = 0
        break
-     else: 
-      print("NO") 
-      break 
+  if ok == 1:
+    print("YES")
+  else :
+    print("NO")
